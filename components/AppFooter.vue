a3fd540a7f02f6c2e07b6e1b4df90f7636d44dba
Carles Ibáñez Golfe <carles2612@gmail.com>
2020-03-23 17:22:05 +0100

    Add Footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="footer">
+    <hr>
+    <div class="container footer-items">
+      <div class="footer-logo">
+        <img src="../assets/img/silla-ajuntament.gif" alt="Ajuntament de Silla">
+      </div>
+      <div class="footer-info">
+        <p class="font-weight-bold">
+          Ajuntament de Silla
+        </p>
+        <p>Plaça del Poble 1, 46460 Silla - Apartat de correus 399. - Tel: 96 120 01 16 - Fax: 96 121 22 23</p>
+        <p>Incidències i ajuda: <a href="mailto:incidencies@sillaparticipa.com">incidencies@sillaparticipa.com</a></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  background: white;
+  margin-top: 1rem;
+
+  &-items {
+    padding: 2.5rem 0;
+    display: flex;
+    align-items: center;
+
+  img {
+    width: 140px;
+    margin-right: 2rem;
+  }
+
+    p {
+      font-size: 1rem;
+      margin: 0 ;
+    }
+  }
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,18 @@
   <div>
     <app-nav />
     <nuxt />
+    <app-footer />
   </div>
 </template>
 
 <script>
 import AppNav from '../components/AppNav'
+import AppFooter from '../components/AppFooter'
+
 export default {
   components: {
-    AppNav
+    AppNav,
+    AppFooter
   }
 }
 </script>
